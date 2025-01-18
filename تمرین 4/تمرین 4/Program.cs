8be4428315584227b85307792e1b5433bfa22b4e
Amirabazarnjad <kamrankamrany135@gmail.com>
2025-01-18 23:32:47 +0330

    Add files via upload

---
+++ تمرین 4/تمرین 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // دریافت عدد تک رقمی از کاربر
+        Console.Write("Enter a single-digit number: ");
+        int digit = Convert.ToInt32(Console.ReadLine());
+
+        // معادل لاتین عدد تک رقمی
+        string[] latinDigits = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+
+        if (digit >= 0 && digit <= 9)
+        {
+            // چاپ معادل لاتین عدد تک رقمی
+            Console.WriteLine("The Latin equivalent is: " + latinDigits[digit]);
+        }
+        else
+        {
+            Console.WriteLine("Please enter a single-digit number between 0 and 9.");
+        }
+    }
+}
+
---
+++ تمرین 5/تمرین 5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // تعداد اعداد را از کاربر دریافت کن
+        Console.Write("Enter the number of values: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        // محاسبه جمع اعداد وارد شده
+        double sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"Enter number {i}: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            sum += number;
+        }
+
+        // محاسبه میانگین
+        double average = sum / n;
+
+        // چاپ میانگین
+        Console.WriteLine("The average of the entered numbers is: " + average);
+    }
+}
